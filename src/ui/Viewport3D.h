d39db11e670e4ff0efdec68d5d349d494f5eda67
ty-ahn <wm@autec.co.kr>
2026-09-18 16:42:59 +0900

    feat: separate OpenGL machine drawing path

--- src/ui/Viewport3D.h
+++ src/ui/Viewport3D.h
@@ -11,7 +11,7 @@
 class Viewport3D final : public QOpenGLWidget, protected QOpenGLFunctions_4_5_Core {
  cnc::Runtime* runtime_{};
  QPoint last_mouse_{}; float yaw_=35.0f,pitch_=25.0f,zoom_=1.0f; QOpenGLShaderProgram* shader_{}; QOpenGLBuffer vbo_{QOpenGLBuffer::VertexBuffer}; QOpenGLVertexArrayObject vao_;
- void drawMachine(QPainter& p,int cx,int cy); 
+ void drawMachine(QPainter& p,int cx,int cy); void drawOpenGLMachine(); 
 public:
  explicit Viewport3D(cnc::Runtime* runtime,QWidget* parent=nullptr):QOpenGLWidget(parent),runtime_(runtime){}
 protected:
