8ca87acc40bba9dcc5d0dcc43bb6ac4ce3a4e76c
ty-ahn <wm@autec.co.kr>
2026-09-18 15:21:32 +0900

    feat: allow motion planner to validate machine-specific limits

--- src/cnc/Motion.h
+++ src/cnc/Motion.h
@@ -6,7 +6,7 @@ namespace cnc {
 struct MotionPoint { double X{},Y{},Z{},A{},C{}; };
 class MotionPlanner {
 public:
- static bool plan(const MotionPoint&,const MotionPoint&,bool,double,std::vector<MotionPoint>&,std::string&,int=20);
+ static bool plan(const MotionPoint&,const MotionPoint&,bool,double,std::vector<MotionPoint>&,std::string&,int=20,const Kinematics* kinematics=nullptr);
  static double rotary_distance(double,double);
  static double linear_length(const MotionPoint&,const MotionPoint&);
 };
