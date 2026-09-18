62abf374e03898810fca19c0c04d36f130f9372f
ty-ahn <wm@autec.co.kr>
2026-09-18 16:40:08 +0900

    feat: prepare OpenGL mesh renderer state

--- src/ui/Viewport3D.h
+++ src/ui/Viewport3D.h
@@ -7,7 +7,7 @@
 #include "cnc/Runtime.h"
 class Viewport3D final : public QOpenGLWidget, protected QOpenGLFunctions_4_5_Core {
  cnc::Runtime* runtime_{};
- QPoint last_mouse_{}; float yaw_=35.0f,pitch_=25.0f,zoom_=1.0f;
+ QPoint last_mouse_{}; float yaw_=35.0f,pitch_=25.0f,zoom_=1.0f; QOpenGLShaderProgram* shader_{}; GLuint vbo_{};
  void drawMachine(QPainter& p,int cx,int cy); 
 public:
  explicit Viewport3D(cnc::Runtime* runtime,QWidget* parent=nullptr):QOpenGLWidget(parent),runtime_(runtime){}
