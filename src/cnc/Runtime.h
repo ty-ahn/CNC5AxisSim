df6fb5deec24370df55cb9d42408738b14a77f7d
ty-ahn <wm@autec.co.kr>
2026-09-18 14:20:03 +0900

    feat: expand Siemens 840D modal runtime

--- src/cnc/Runtime.h
+++ src/cnc/Runtime.h
@@ -1,19 +1,37 @@
 #pragma once
 #include "Kinematics.h"
 #include "Parser.h"
+#include <string>
+
 namespace cnc {
+
+enum class MotionMode { Rapid, Linear, ArcCW, ArcCCW };
+
+struct ModalState {
+    MotionMode motion{MotionMode::Rapid};
+    bool absolute{true};
+    int work_offset{54};
+    int plane{17};
+    bool coolant{false};
+    int spindle_direction{0};
+};
+
 class Runtime {
     MachineState state_{};
-    bool absolute_=true;
-    double feed_=0,rpm_=0;
-    int tool_=0,d_=0;
+    ModalState modal_{};
+    double feed_{0}, rpm_{0};
+    int tool_{0}, d_{0};
+    double work_x_{0}, work_y_{0}, work_z_{0};
+
 public:
-    bool execute(const Block&,std::string&);
-    const MachineState& state() const{return state_;}
-    double feed() const{return feed_;}
-    double rpm() const{return rpm_;}
-    int tool() const{return tool_;}
-    int d() const{return d_;}
+    bool execute(const Block&, std::string&);
+    const MachineState& state() const { return state_; }
+    const ModalState& modal() const { return modal_; }
+    double feed() const { return feed_; }
+    double rpm() const { return rpm_; }
+    int tool() const { return tool_; }
+    int d() const { return d_; }
     void reset();
 };
+
 }
