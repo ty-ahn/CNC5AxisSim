f8039f68b4797f2e06d9f6f291ebd44356fe9305
ty-ahn <wm@autec.co.kr>
2026-09-18 14:21:09 +0900

    feat: add 5-axis motion interpolation

---
+++ src/cnc/Motion.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Kinematics.h"
+#include <string>
+#include <vector>
+namespace cnc {
+struct MotionPoint { double X{},Y{},Z{},A{},C{}; };
+class MotionPlanner {
+public:
+ static bool plan(const MotionPoint&,const MotionPoint&,bool,double,std::vector<MotionPoint>&,std::string&,int=20);
+ static double rotary_distance(double,double);
+ static double linear_length(const MotionPoint&,const MotionPoint&);
+};
+}
