b85676ad3ff5c09de8afaa281d175bfefbe74c22
ty-ahn <wm@autec.co.kr>
2026-09-18 16:01:41 +0900

    feat: expose stock signed-distance query

--- src/cnc/Stock.h
+++ src/cnc/Stock.h
@@ -6,7 +6,7 @@ enum class ToolShape { Flat, Ball, BullNose };
 enum class CollisionType { ToolStock, HolderStock, HolderMachine };
 struct ToolGeometry { ToolShape shape{ToolShape::Flat}; double radius{2}; double corner_radius{0}; double length{100}; double holder_radius{20}; double holder_length{80}; };
 struct StockDefinition { Vec3 origin{}; double size_x{100},size_y{100},size_z{50}; double resolution{2}; };
-struct StockCell { bool removed{false}; };
+struct StockCell { bool removed{false}; float sdf{0}; };
 struct CollisionEvent { CollisionType type{}; Vec3 position{}; double penetration{}; bool collision{false}; };
 class Stock {
  StockDefinition def_{}; int nx_{},ny_{},nz_{}; std::vector<StockCell> cells_;
@@ -21,7 +21,7 @@ public:
  CollisionEvent check_holder_stock(const Vec3& holder_start,const Vec3& holder_end,double radius) const;
  size_t cell_count()const{return cells_.size();}
  bool is_removed(int x,int y,int z)const;
- double remaining_volume() const;
+ double remaining_volume() const;\n double signed_distance(const Vec3& p) const;
  std::vector<Vec3> removed_centers() const;
 };
 }
