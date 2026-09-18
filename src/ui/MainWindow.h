e167aa2acadcdfcbc24fb59ede937160f064157c
ty-ahn <wm@autec.co.kr>
2026-09-18 20:13:24 +0900

    feat: add machine tree panel to UI

--- src/ui/MainWindow.h
+++ src/ui/MainWindow.h
@@ -2,13 +2,13 @@
 #include "cnc/OperatorController.h"
 #include "cnc/ProgramCatalog.h"
 #include <QMainWindow>
-class QPlainTextEdit; class QLabel; class QPushButton; class QListWidget;
+class QPlainTextEdit; class QLabel; class QPushButton; class QListWidget; class QTreeWidget; class Viewport3D;
 class MainWindow final : public QMainWindow {
  Q_OBJECT
  cnc::Runtime runtime_; cnc::OperatorController operator_; cnc::ProgramCatalog catalog_;
  QPlainTextEdit* program_{};
  QLabel *x_{},*y_{},*z_{},*a_{},*c_{},*feed_{},*rpm_{},*status_{};
- QListWidget* alarms_{};
+ QListWidget* alarms_{}; QTreeWidget* machine_tree_{}; Viewport3D* viewport_{};
  void refresh();
  void loadDemo();
 public:
