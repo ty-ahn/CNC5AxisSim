a0827f0f60c1c0d8813d228ed1d81deaefe80d26
ty-ahn <wm@autec.co.kr>
2026-09-18 14:22:29 +0900

    feat: add TRAORI modal state

--- src/cnc/Runtime.h
+++ src/cnc/Runtime.h
@@ -5,7 +5,7 @@
 #include <vector>
 namespace cnc {
 enum class MotionMode{Rapid,Linear,ArcCW,ArcCCW};
-struct ModalState{MotionMode motion{MotionMode::Rapid};bool absolute{true};int work_offset{54};int plane{17};int feed_mode{94};bool coolant{false};int spindle_direction{0};};
+struct ModalState{MotionMode motion{MotionMode::Rapid};bool absolute{true};int work_offset{54};int plane{17};int feed_mode{94};bool coolant{false}; bool traori{false};int spindle_direction{0};};
 class Runtime{
  MachineState state_{};ModalState modal_{};double feed_{},rpm_{};int tool_{},d_{};std::vector<MotionPoint> last_motion_;
 public:bool execute(const Block&,std::string&);const MachineState& state()const{return state_;}const ModalState& modal()const{return modal_;}const std::vector<MotionPoint>& last_motion()const{return last_motion_;}double feed()const{return feed_;}double rpm()const{return rpm_;}int tool()const{return tool_;}int d()const{return d_;}void reset();
