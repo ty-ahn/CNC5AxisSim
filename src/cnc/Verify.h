cbb06443f1b3a9a6fd6e68c9a90b61c4e43f33e3
ty-ahn <wm@autec.co.kr>
2026-09-18 21:05:58 +0900

    fix: repair verify result header

--- src/cnc/Verify.h
+++ src/cnc/Verify.h
@@ -3,7 +3,20 @@
 #include "Runtime.h"
 #include <unordered_map>
 namespace cnc {
-struct VerifyResult { bool passed{}; size_t executed{}; size_t errors{}; size_t collisions{}; size_t steps{}; std::string first_error; MachineState final_state{}; double remaining_volume{};\n size_t tool_stock_collisions{};\n size_t holder_stock_collisions{};\n size_t holder_machine_collisions{};\n double max_penetration{}; };
+struct VerifyResult {
+ bool passed{};
+ size_t executed{};
+ size_t errors{};
+ size_t collisions{};
+ size_t steps{};
+ std::string first_error;
+ MachineState final_state{};
+ double remaining_volume{};
+ size_t tool_stock_collisions{};
+ size_t holder_stock_collisions{};
+ size_t holder_machine_collisions{};
+ double max_penetration{};
+};
 class VerifyEngine {
 public:
  static VerifyResult run(const std::vector<Block>& blocks, Runtime runtime={});
