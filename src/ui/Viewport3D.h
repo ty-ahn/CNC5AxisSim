a8d67f0219416cb276f80ba72d1a9c6685f3896e
ty-ahn <wm@autec.co.kr>
2026-09-18 14:51:48 +0900

    feat: prepare viewport toolpath state

--- src/ui/Viewport3D.h
+++ src/ui/Viewport3D.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions_4_5_Core>
+#include <vector>
 #include "cnc/Runtime.h"
 class Viewport3D final : public QOpenGLWidget, protected QOpenGLFunctions_4_5_Core {
- cnc::Runtime* runtime_{};
+ cnc::Runtime* runtime_{}; std::vector<QPointF> path_;
 public:
  explicit Viewport3D(cnc::Runtime* runtime,QWidget* parent=nullptr):QOpenGLWidget(parent),runtime_(runtime){}
 protected:
