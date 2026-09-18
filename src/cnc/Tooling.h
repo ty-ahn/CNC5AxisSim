079127b5785d951255c423fdf3eef8135f4741ca
ty-ahn <wm@autec.co.kr>
2026-09-18 16:04:45 +0900

    feat: add holder geometry to tool definitions

--- src/cnc/Tooling.h
+++ src/cnc/Tooling.h
@@ -1,11 +1,19 @@
 #pragma once
-#include <unordered_map>
+#include <unordered_map>\n#include <string>
 namespace cnc {
-struct ToolDefinition { int number{}; double length{}; double diameter{}; double corner_radius{}; };
+struct ToolDefinition {
+ int number{};
+ double length{};
+ double diameter{};
+ double corner_radius{};
+ double holder_diameter{20.0};
+ double holder_length{80.0};
+};
 class ToolTable {
  std::unordered_map<int,ToolDefinition> tools_;
 public:
  void set(const ToolDefinition& t){tools_[t.number]=t;}
  bool get(int n,ToolDefinition& t) const {auto i=tools_.find(n);if(i==tools_.end())return false;t=i->second;return true;}
+ size_t size() const{return tools_.size();}
 };
 }
