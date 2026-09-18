48abe10a6b7f520928fdba2bea1b4ec0b87736fb
ty-ahn <wm@autec.co.kr>
2026-09-18 14:48:54 +0900

    feat: add Qt6 CNC operator main window

---
+++ src/ui/MainWindow.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "cnc/OperatorController.h"
+#include <QMainWindow>
+class QPlainTextEdit; class QLabel; class QPushButton; class QListWidget;
+class MainWindow final : public QMainWindow {
+ Q_OBJECT
+ cnc::Runtime runtime_; cnc::OperatorController operator_;
+ QPlainTextEdit* program_{};
+ QLabel *x_{},*y_{},*z_{},*a_{},*c_{},*feed_{},*rpm_{},*status_{};
+ QListWidget* alarms_{};
+ void refresh();
+ void loadDemo();
+public:
+ explicit MainWindow(QWidget* parent=nullptr);
+};
