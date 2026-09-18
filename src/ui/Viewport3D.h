75691b193eb6bd4fc5086571b5546795c65b60c9
ty-ahn <wm@autec.co.kr>
2026-09-18 14:53:32 +0900

    feat: add interactive 3D viewport controls

--- src/ui/Viewport3D.h
+++ src/ui/Viewport3D.h
@@ -2,12 +2,18 @@
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions_4_5_Core>
 #include <vector>
+#include <QMouseEvent>
+#include <QWheelEvent>
 #include "cnc/Runtime.h"
 class Viewport3D final : public QOpenGLWidget, protected QOpenGLFunctions_4_5_Core {
- cnc::Runtime* runtime_{}; 
+ cnc::Runtime* runtime_{};
+ QPoint last_mouse_{}; float yaw_=35.0f,pitch_=25.0f,zoom_=1.0f; 
 public:
  explicit Viewport3D(cnc::Runtime* runtime,QWidget* parent=nullptr):QOpenGLWidget(parent),runtime_(runtime){}
 protected:
+ void mousePressEvent(QMouseEvent*) override;
+ void mouseMoveEvent(QMouseEvent*) override;
+ void wheelEvent(QWheelEvent*) override;
  void initializeGL() override;
  void resizeGL(int w,int h) override;
  void paintGL() override;
