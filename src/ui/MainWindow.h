8be28e792a9d5786eebd1445f758dc697e4691d9
ty-ahn <wm@autec.co.kr>
2026-09-18 15:25:19 +0900

    feat: add program catalog to Qt operator

--- src/ui/MainWindow.h
+++ src/ui/MainWindow.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "cnc/OperatorController.h"
+#include "cnc/ProgramCatalog.h"
 #include <QMainWindow>
 class QPlainTextEdit; class QLabel; class QPushButton; class QListWidget;
 class MainWindow final : public QMainWindow {
  Q_OBJECT
- cnc::Runtime runtime_; cnc::OperatorController operator_;
+ cnc::Runtime runtime_; cnc::OperatorController operator_; cnc::ProgramCatalog catalog_;
  QPlainTextEdit* program_{};
  QLabel *x_{},*y_{},*z_{},*a_{},*c_{},*feed_{},*rpm_{},*status_{};
  QListWidget* alarms_{};
